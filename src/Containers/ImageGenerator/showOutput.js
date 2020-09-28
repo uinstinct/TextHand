6055b171aadbebaa2e175ab3e4db6a9d5a454a70
uinstinct <uinstinct@outlook.com>
2020-09-29 00:57:28 +0530

    grid layout added for displaying images

--- src/Containers/ImageGenerator/showOutput.js
+++ src/Containers/ImageGenerator/showOutput.js
@@ -1,22 +1,39 @@
-import React, { useMemo } from 'react';
-import { Image } from 'semantic-ui-react';
+import React, { useMemo, useContext } from 'react';
+import { Image, Segment, Grid, GridRow, GridColumn } from 'semantic-ui-react';
+import { DarkTheme } from '../../Themes';
 
 function ShowOutput(props) {
+    const { isActive } = useContext(DarkTheme);
+
     const images = useMemo(() => {
         if (props.images.length > 0) {
-            const tempImages = props.images.map(image => {
-                return <Image src={image.toDataURL()} />
-            })
+            let tempImages = [];
+            for (let i = 0; i < props.images.length; i += 4) {
+                let row = [];
+                for (let j = i; j < props.images.length && j < i + 4; j++) {
+                    const col =
+                        <GridColumn>
+                            <Image src={props.images[j].toDataURL()} size='large' />
+                        </GridColumn>;
+                    row.push(col);
+                }
+                const tempImageRow = <GridRow columns={4}>{row}</GridRow>
+                tempImages.push(tempImageRow);
+            }
             return tempImages;
         } else return [];
     }, [props.images]);
+
     return (
         <>
-            {images.length > 0 ?
-                <>Working
-                    {images}
-                </>
-                : "Start Generating"}
+            <Segment inverted={isActive} >
+                <Grid>
+                    {images.length > 0 ?
+                        <>{images}</>
+                        : "Start Generating"
+                    }
+                </Grid>
+            </Segment>
         </>
     );
 }
