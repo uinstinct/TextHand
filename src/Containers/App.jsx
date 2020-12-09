e7196fab1b429361407a979a0c1dd326fbe06b6f
uinstinct <uinstinct@outlook.com>
2020-12-09 12:25:34 +0530

    add apple-touch-icon of 180x180 png image fix initial darkmode completed PWA serviceWorker registered in public url

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -4,11 +4,28 @@ import 'semantic-ui-css/semantic.min.css';
 import { DarkTheme, } from 'Themes/index';
 
 import Navbar from 'Containers/Navbar';
-import Main from './Main';
+import Main from 'Containers/Main';
 
 export default function App() {
-    const storedMode = localStorage.getItem('darkmode') === 'true';
-    const [darkmode, setDarkmode] = useState(storedMode);
+    const storedMode = localStorage.getItem('darkmode');
+    const initialMode = storedMode ? (storedMode === 'true') : true;
+    const [darkmode, setDarkmode] = useState(initialMode);
+
+     useEffect(() => {
+         window.addEventListener('load', () => {
+             if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+                navigator.serviceWorker
+                    .register(process.env.PUBLIC_URL + '/sw.js',
+                        { scope: '/' })
+                    .then(() => {
+                        console.log('ServiceWorker Registered');
+                    })
+                    .catch(() => {
+                        console.log('Error while registering service workers');
+                    });
+            }
+        });
+     }, []);
 
     useEffect(() => {
         localStorage.setItem('darkmode', darkmode);
